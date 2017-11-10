b73d7e83312c7060ba96970274c53087580a661a
Feliks Gurevich <feliks@twimco.com>
2017-11-10 02:19:10 -0500

    bumping to beta 21

--- src/angular2-token.interceptor.ts
+++ src/angular2-token.interceptor.ts
@@ -1,19 +1,15 @@
-import { Inject, Injectable, forwardRef } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Angular2TokenService } from './angular2-token.service';
 
 @Injectable()
 export class Angular2TokenInterceptor implements HttpInterceptor {
-    authService: Angular2TokenService;
-
-    constructor(@Inject(forwardRef(() => Angular2TokenService)) angular2TokenService) {
-        this.authService = angular2TokenService;
-    }
+    constructor(private _angular2TokenService: Angular2TokenService ) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        this.authService.setCurrentAuthHeaders();
-        const authHeaders = this.authService.currentAuthHeaders;
+        this._angular2TokenService.setCurrentAuthHeaders();
+        const authHeaders = this._angular2TokenService.currentAuthHeaders;
         authHeaders.keys().forEach((key) => req.headers.append(key, authHeaders.get(key)));
         const authReq = req.clone({ headers: req.headers });
         return next.handle(authReq);
