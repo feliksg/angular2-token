c267d67383e8da9d82447409743da01cafb7482f
Feliks Gurevich <feliks@twimco.com>
2017-11-10 01:44:34 -0500

    bumping to beta 18

--- lib/src/angular2-token.interceptor.d.ts
+++ lib/src/angular2-token.interceptor.d.ts
@@ -1,7 +1,7 @@
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Angular2TokenService } from './angular2-token.service';
-export declare class AuthInterceptor implements HttpInterceptor {
+export declare class Angular2TokenInterceptor implements HttpInterceptor {
     private _angular2TokenService;
     constructor(_angular2TokenService: Angular2TokenService);
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>;
